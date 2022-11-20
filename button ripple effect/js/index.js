36dbfa60cc1521ce048a21ea9bc08052887b6439
mohamed hassan wazer <118473437+mohamed-hassan-wazer@users.noreply.github.com>
2022-11-21 01:00:09 +0200

    x for get the horizontal coordinate from left to the button

--- button ripple effect/js/index.js
+++ button ripple effect/js/index.js
@@ -1,7 +1,7 @@
 const btnEl = document.querySelector(".btn");
 
-//btnEl.addEventListener("mouseover", (event) => {
-  const x = event.pageX - btnEl.offsetLeft;
+btnEl.addEventListener("mouseover", (event) => {
+  //const x = event.pageX - btnEl.offsetLeft;
   const y = event.pageY - btnEl.offsetTop;
 
   btnEl.style.setProperty("--xPos", x + "px");
